c25e6c0037c23c9ec5a88c0da8620f474fc289f1
Juan David Osorio Ortiz <59321452+JuanDa15@users.noreply.github.com>
2021-03-20 13:08:34 -0500

    xd

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/DLmanager'));
+app.use(express.static(__dirname+'/dist/dlmanager'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/DLmanager/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/dlmanager/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
