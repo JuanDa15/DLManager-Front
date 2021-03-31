a6f983d79a832d18ec6394f5ddc5fab2a66fdb58
Juan David Osorio Ortiz <59321452+JuanDa15@users.noreply.github.com>
2021-03-31 01:23:13 -0500

    version 0.9

--- src/app/ayuda/ayuda.component.ts
+++ src/app/ayuda/ayuda.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ayuda',
   templateUrl: './ayuda.component.html',
   styleUrls: ['./ayuda.component.scss']
 })
-export class AyudaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AyudaComponent{
 }
